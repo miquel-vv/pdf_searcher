07e258bc1bb71ceecf4fc6d7941c902fa92aef00
Miquel Vande Velde <miquel.vandevelde@gmail.com>
2018-09-30 13:32:08 +0100

    Create the PdfSearcher object and all its methods.
    Implemented the object with methods: search_pdf, match_pattern and to_csv.

---
+++ pdf_searcher.py
@@ -0,0 +1,44 @@
+import PyPDF2
+import re
+import csv
+from collections import namedtuple 
+Record = namedtuple('Record', 'file_path, word, page, occurence')
+
+class PdfSearcher:
+    """The pdf searcher is an object wich stores the desired search words but not the pdf and is as such
+    designed to be used on multiple pdf's. It stores all found words in the pdf it has looked in 
+    (using the search_pdf method) as records. A record contains info about: file path of the pdf,
+    the word that was found, the page on which it was found and the amount of times it appeared on 
+    that page."""
+
+    def __init__(self, *args):
+        """The positional arguments passed to initiliase the object are the search words it will use."""
+        self.search_words = [re.compile(arg) for arg in args]
+        self.records = []
+
+    def search_pdf(self, pdf_path):
+        """Creates a record for each word found on each page. Scans the pdf file (passed through 
+        with full path) and calls the match patterns method for each page. Each match that match_patterns
+        returns is stored as a record in self.records."""
+        with open(pdf_path, 'rb') as pdf:
+            pdf_reader = PyPDF2.PdfFileReader(pdf)
+            for page_num, page in enumerate(pdf_reader.pages, 1):
+                text = page.extractText()
+                for found in self.match_patterns(text):
+                    self.records.append(Record(pdf_path, found[0], page_num, len(found)))
+
+    def match_patterns(self, text):
+        """Creates a generator that looks for all search words in a given text."""
+        for word in self.search_words:
+            match = word.findall(text)
+            if match:
+                yield match
+
+    def to_csv(self, file_path, first=True):
+        """Creates or appends a csv file containing all records that were found."""
+        with open(file_path, 'a') as file:
+            writer = csv.writer(file, delimiter=",")
+            if first:
+                writer.writerow(['file_path', 'word', 'page', 'occurence'])
+            for record in self.records:
+                writer.writerow([record.file_path, record.word, record.page, record.occurence])
