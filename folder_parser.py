c8cd7ee19c9ef5dbb5c69c9567d1277f96460aa2
Miquel Vande Velde <miquel.vandevelde@gmail.com>
2018-09-30 18:43:04 +0100

    Add error handling for paths provided.

--- folder_parser.py
+++ folder_parser.py
@@ -11,9 +11,9 @@ def find_pdfs(top_folder_path):
         raise ValueError('The passed path is not a valid folder.')
 
     pdf_list = []
-    for top_folder, dirs, files in os.walk(top_folder_path):
+    for top_folder, _, files in os.walk(top_folder_path):
         for file in files:
-            name, file_type = os.path.splitext(os.path.join(top_folder,file))
+            _, file_type = os.path.splitext(os.path.join(top_folder,file))
             if file_type == '.pdf':
                 pdf_list.append(os.path.join(top_folder,file))
     
