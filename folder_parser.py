0f2a376ddfd0855674a140708c112e46e8847b85
Miquel Vande Velde <miquel.vandevelde@gmail.com>
2018-09-30 18:20:25 +0100

    Change the folder parser to return a list rather than a generator.
    To track progress and allow possible multi-threading in the future, the full list is returned rather than
    yielding the paths one by one.

--- folder_parser.py
+++ folder_parser.py
@@ -3,13 +3,15 @@ import logging
 
 logging.basicConfig(filename='structure.txt', level=logging.DEBUG)
 
-def find_pdfs(top_folder_path, fn):
+def find_pdfs(top_folder_path, print_only=True):
     """Walks through folder structure and calls the passed function on each pdf.
     Accepts the top file directory as first argument and a callback function as the second. The callback
     function should accept one argument: The pdf's absolute path."""
+    pdf_list = []
     for top_folder, dirs, files in os.walk(top_folder_path):
         for file in files:
             name, file_type = os.path.splitext(os.path.join(top_folder,file))
             if file_type == '.pdf':
-                fn(os.path.join(top_folder,file))
-                logging.info(os.path.join(top_folder,file))
+                pdf_list.append(os.path.join(top_folder,file))
+    
+    return pdf_list
